582bd1d7bc067088b3e207ba031b8e27f91e09fb
Himanshu Shukla <47292260+shukla-19@users.noreply.github.com>
2026-08-05 20:12:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('.card').forEach(c=>c.addEventListener('mouseenter',()=>c.style.boxShadow='0 12px 32px rgba(0,0,0,.08)'));document.querySelectorAll('.card').forEach(c=>c.addEventListener('mouseleave',()=>c.style.boxShadow='none'));
